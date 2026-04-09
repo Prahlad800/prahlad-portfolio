279104d9d68c5264ae9b6f2318f8b4deb4efd5eb
prahlad800 <prahlad8009872468@gmail.com>
2026-04-09 22:33:23 +0530

    backend complete in portfolio

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors";
+import routerComment from "./router/comment_router.js";
 
 import { connectdb } from "./db/db.js"
-
-
-
-
 dotenv.config()
 connectdb()
 const port = process.env.PORT
 const app = express()
-app.get("/",(req,res)=>{
+
+app.use(cors());
+app.use(express.json());
+app.get("/hello",(req,res)=>{
     res.send("my portfolio")
 })
+
+app.use("/contact",routerComment)
+
+
 app.listen(port,()=>{
     console.log(`app run ${port}`)
 })
