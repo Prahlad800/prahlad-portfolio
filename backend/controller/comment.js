4d56340dd2ebed9850abc44a359c338c7e870bd2
prahlad800 <prahlad8009872468@gmail.com>
2026-04-15 00:13:42 +0530

    fix

--- backend/controller/comment.js
+++ backend/controller/comment.js
@@ -17,30 +17,30 @@ export const submitContact = async (req, res) => {
         });
 
         // 👉 2. Email Setup
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            host: "smtp.gmail.com",   
-            port: 587,                
-            secure: false,            
-            family: 4,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
-            }
-        });
+        // const transporter = nodemailer.createTransport({
+        //     service: "gmail",
+        //     host: "smtp.gmail.com",   
+        //     port: 587,                
+        //     secure: false,            
+        //     family: 4,
+        //     auth: {
+        //         user: process.env.EMAIL_USER,
+        //         pass: process.env.EMAIL_PASS
+        //     }
+        // });
 
         // 👉 3. Send Email to YOU
-        await transporter.sendMail({
-            from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
-            to: "prahlad0227@gmail.com",
-            subject: "🚀 New Contact Message आया है",
-            html: `
-                <h2>New Contact Message</h2>
-                <p><b>Name:</b> ${name}</p>
-                <p><b>Email:</b> ${email}</p>
-                <p><b>Comment:</b> ${comment}</p>
-            `
-        });
+        // await transporter.sendMail({
+        //     from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
+        //     to: "prahlad0227@gmail.com",
+        //     subject: "🚀 New Contact Message आया है",
+        //     html: `
+        //         <h2>New Contact Message</h2>
+        //         <p><b>Name:</b> ${name}</p>
+        //         <p><b>Email:</b> ${email}</p>
+        //         <p><b>Comment:</b> ${comment}</p>
+        //     `
+        // });
 
         res.status(200).json({
             success: true,
